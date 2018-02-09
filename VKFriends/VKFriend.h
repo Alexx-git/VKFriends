d0fef2b69a776396db8f3ea882bd61cac3837192
VLADIMIR <vlad.dudkin@gmail.com>
2018-02-09 20:02:13 +0200

    Login screen deleted. Friend model added

---
+++ VKFriends/VKFriend.h
@@ -0,0 +1,18 @@
+//
+//  VKFriend.h
+//  VKFriends
+//
+//  Created by VLADIMIR on 2/9/18.
+//  Copyright © 2018 ALEXANDER. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface VKFriend : NSObject
+
+@property(strong, nonatomic) NSString * name;
+@property(strong, nonatomic) NSString * surname;
+@property(strong, nonatomic) UIImage * photo;
+
+@end
---
+++ VKFriends/VKFriend.m
@@ -0,0 +1,13 @@
+//
+//  VKFriend.m
+//  VKFriends
+//
+//  Created by VLADIMIR on 2/9/18.
+//  Copyright © 2018 ALEXANDER. All rights reserved.
+//
+
+#import "VKFriend.h"
+
+@implementation VKFriend
+
+@end
